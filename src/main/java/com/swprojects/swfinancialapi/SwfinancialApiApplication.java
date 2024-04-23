1367df1181997f4efca470346026b7e6fd3f2735
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-04-22 21:39:46 -0300

    README.md

---
+++ src/main/java/com/swprojects/swfinancialapi/SwfinancialApiApplication.java
@@ -0,0 +1,13 @@
+package com.swprojects.swfinancialapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwfinancialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwfinancialApiApplication.class, args);
+	}
+
+}
