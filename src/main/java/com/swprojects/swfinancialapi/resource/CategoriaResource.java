6ee759a8730571995877d488384aace99c581db2
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-04-17 21:23:20 -0300

    Cadastro de novas categorias com POST Retornando 404 caso não exista categoria

--- src/main/java/com/swprojects/swfinancialapi/resource/CategoriaResource.java
+++ src/main/java/com/swprojects/swfinancialapi/resource/CategoriaResource.java
@@ -1,14 +1,23 @@
 package com.swprojects.swfinancialapi.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.swprojects.swfinancialapi.model.Categoria;
 import com.swprojects.swfinancialapi.repositories.CategoriaRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/categorias")
@@ -20,5 +29,24 @@ public class CategoriaResource {
   @GetMapping
   public List<Categoria> listar() {
     return categoriaRepository.findAll();
+  }
+
+  @PostMapping
+  public ResponseEntity<Categoria> adicionar(@RequestBody Categoria categoria, HttpServletResponse response) {
+    Categoria novaCategoria = categoriaRepository.save(categoria);
+
+    URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+        .path("/{codigo}")
+        .buildAndExpand(novaCategoria.getCodigo())
+        .toUri();
+    response.setHeader("Location", uri.toASCIIString());
+    return ResponseEntity.status(HttpStatus.CREATED).body(novaCategoria);
+  }
+
+  @GetMapping("/{codigo}")
+  public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+    return categoriaRepository.findById(codigo)
+        .map(categoria -> ResponseEntity.ok(categoria))
+        .orElse(ResponseEntity.notFound().build());
   }
 }
