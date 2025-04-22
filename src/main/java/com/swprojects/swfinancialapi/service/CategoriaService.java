a72180ae27badd960089d9e0818d8cb04d51d23c
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-04-22 11:32:10 -0300

    CategoriaService implementado

---
+++ src/main/java/com/swprojects/swfinancialapi/service/CategoriaService.java
@@ -0,0 +1,31 @@
+package com.swprojects.swfinancialapi.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.swprojects.swfinancialapi.model.Categoria;
+import com.swprojects.swfinancialapi.repositorie.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+  
+  @Autowired
+  private CategoriaRepository categoriaRepository;
+
+  public Categoria salvar(Categoria categoria) {
+    return categoriaRepository.save(categoria);
+  }
+
+  @Transactional(readOnly = true)
+  public List<Categoria> listarTodos() {
+    return categoriaRepository.findAll();
+  }
+
+  public Optional<Categoria> buscarCategoriaPeloCodigo(Long codigo) {
+    return categoriaRepository.findById(codigo);
+  }
+}
