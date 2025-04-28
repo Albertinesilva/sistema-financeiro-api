904c3f9244ad2a8873b39f8804cd131a2eece8e3
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-04-28 15:48:40 -0300

    Comentarios deletados

--- src/main/java/com/swprojects/swfinancialapi/config/AuthorizationServerConfig.java
+++ src/main/java/com/swprojects/swfinancialapi/config/AuthorizationServerConfig.java
@@ -30,14 +30,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
   @Override
   public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-    // clients.inMemory()
-    // .withClient("angular")
-    // .secret(passwordEncoder.encode("@ngul@r0")) // @ngul@r0
-    // .scopes("read", "write")
-    // .authorizedGrantTypes("password", "refresh_token")
-    // .accessTokenValiditySeconds(20)
-    // .refreshTokenValiditySeconds(3600 * 24);
-
     clients.inMemory()
         .withClient("angular")
         .secret(passwordEncoder.encode("@ngul@r0")) // @ngul@r0
