e921c46742ea8f6ab77a988ce3d839ef45831a8b
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2025-04-28 11:21:29 -0300

    Versão 2.7.0 adicionada no projeto para configuração do spring security com Tokens JWT

--- src/main/java/com/swprojects/swfinancialapi/config/AuthorizationServerConfig.java
+++ src/main/java/com/swprojects/swfinancialapi/config/AuthorizationServerConfig.java
@@ -4,15 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
@@ -23,19 +25,33 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
   @Autowired
   private PasswordEncoder passwordEncoder;
 
+  @Autowired
+  private UserDetailsService userDetailsService;
+
+  @Override
   public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    // clients.inMemory()
+    // .withClient("angular")
+    // .secret(passwordEncoder.encode("@ngul@r0")) // @ngul@r0
+    // .scopes("read", "write")
+    // .authorizedGrantTypes("password", "refresh_token")
+    // .accessTokenValiditySeconds(20)
+    // .refreshTokenValiditySeconds(3600 * 24);
+
     clients.inMemory()
         .withClient("angular")
         .secret(passwordEncoder.encode("@ngul@r0")) // @ngul@r0
         .scopes("read", "write")
-        .authorizedGrantTypes("password")
-        .accessTokenValiditySeconds(1800)
+        .authorizedGrantTypes("password", "refresh_token")
+        .accessTokenValiditySeconds(20) // 20 seconds
+        .refreshTokenValiditySeconds(3600 * 24) // 1 day
         .and()
         .withClient("mobile")
         .secret(passwordEncoder.encode("m0b1l30")) // m0b1l30
         .scopes("read")
-        .authorizedGrantTypes("password")
-        .accessTokenValiditySeconds(1800);
+        .authorizedGrantTypes("password", "refresh_token")
+        .accessTokenValiditySeconds(20) // 20 seconds
+        .refreshTokenValiditySeconds(3600 * 24); // 1 day
   }
 
   @Override
@@ -43,15 +59,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     endpoints
         .authenticationManager(authenticationManager)
         .accessTokenConverter(accessTokenConverter())
-        .tokenStore(tokenStore());
+        .tokenStore(tokenStore())
+        .userDetailsService(userDetailsService)
+        .reuseRefreshTokens(false);
   }
 
   @Bean
   public JwtAccessTokenConverter accessTokenConverter() {
     JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
-
     accessTokenConverter.setSigningKey("3032885ba9cd6621bcc4e7d6b6c35c2b");
-
     return accessTokenConverter;
   }
 
--- src/main/java/com/swprojects/swfinancialapi/config/ResourceServerConfig.java
+++ src/main/java/com/swprojects/swfinancialapi/config/ResourceServerConfig.java
@@ -1,22 +1,5 @@
 package com.swprojects.swfinancialapi.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -24,16 +7,66 @@ import java.util.stream.Collectors;
 
 import javax.crypto.spec.SecretKeySpec;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+  // @Override
+  // protected void configure(AuthenticationManagerBuilder auth) throws Exception
+  // {
+  // auth.inMemoryAuthentication()
+  // .withUser("admin@algamoney.com")
+  // .password("admin")
+  // .roles("ROLE");
+  // }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    manager.createUser(User.withUsername("admin@algamoney.com")
+        .password(passwordEncoder().encode("admin"))
+        .roles("ADMIN")
+        .build());
+    return manager;
+  }
 
   @Override
   public void configure(HttpSecurity http) throws Exception {
+
+    // http.authorizeRequests()
+    // .antMatchers("/categorias").permitAll()
+    // .anyRequest().authenticated()
+    // .and()
+    // .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    // .and()
+    // .csrf().disable()
+    // .oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
+
     http
         .authorizeHttpRequests(auth -> auth
             .antMatchers("/categorias").permitAll()
-            .antMatchers("/oauth2/token", "/oauth2/introspect").permitAll()
             .anyRequest().authenticated())
         .sessionManagement(session -> session
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -43,29 +76,44 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
   }
 
   @Bean
+  public JwtDecoder jwtDecoder() {
+    var secretKey = new SecretKeySpec("3032885ba9cd6621bcc4e7d6b6c35c2b".getBytes(), "HmacSHA256");
+    return NimbusJwtDecoder.withSecretKey(secretKey).build();
+  }
+
+  // @Bean
+  // @Override
+  // protected AuthenticationManager authenticationManager() throws Exception {
+  // return super.authenticationManager();
+  // }
+
+  @Bean
   public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-    // Configuração para AuthenticationManager
     AuthenticationManagerBuilder authenticationManagerBuilder = http
         .getSharedObject(AuthenticationManagerBuilder.class);
-    authenticationManagerBuilder.inMemoryAuthentication()
-        .passwordEncoder(passwordEncoder())
-        .withUser("admin@algamoney.com")
-        .password(passwordEncoder().encode("admin"))
-        .roles("ADMIN");
+
+    authenticationManagerBuilder
+        .userDetailsService(userDetailsService()) // Usa o seu bean aqui
+        .passwordEncoder(passwordEncoder());
+
     return authenticationManagerBuilder.build();
   }
 
-  @Bean
-  public JwtDecoder jwtDecoder() {
-    var secretKey = new SecretKeySpec("3032885ba9cd6621bcc4e7d6b6c35c2b".getBytes(), "HmacSHA256");
-
-    return NimbusJwtDecoder.withSecretKey(secretKey).build();
-  }
+  // @Bean
+  // public PasswordEncoder passwordEncoder() {
+  // return NoOpPasswordEncoder.getInstance();
+  // }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder(); // Usando BCrypt para codificação de senhas
   }
+
+  // @Bean
+  // @Override
+  // public UserDetailsService userDetailsServiceBean() throws Exception {
+  // return super.userDetailsServiceBean();
+  // }
 
   private JwtAuthenticationConverter jwtAuthenticationConverter() {
     JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
@@ -88,5 +136,4 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     return jwtAuthenticationConverter;
   }
-
 }
